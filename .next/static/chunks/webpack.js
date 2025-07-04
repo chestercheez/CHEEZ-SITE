1ea2eb0ec265f012ca0096f6295de455cce78d9c
avaxtruthboard <avaxtruthboard@proton.me>
2025-07-04 13:28:52 -0400

    Remove node_modules from repository and add to .gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3f1d6aef6e6d8ea6"; }
+/******/ 		__webpack_require__.h = function() { return "8321e7e098f21fee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
